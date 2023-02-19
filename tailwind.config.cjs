591c3ec0d0ca39a28497f0f9e7679e78b05cc48f
Pakvothe <fr.dv.ortiz@gmail.com>
2023-02-18 21:03:51 -0300

    (configuration) install deps

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
+      ...colors,
       transparent: "transparent",
-      current: "currentColor",
-      background: "#3c3f41",
-      primary: "#2c3e50",
-      secondary: "#2c3e50",
-      accent: "#f7931e",
-      text: "#fafafa",
+      background: "#F7F7F7",
+      primary: "#256D85",
+      secondary: "#2B4865",
+      accent: "#8FE3CF",
+      text: "#002B5B",
     },
   },
   plugins: [],
