3b7ba60f4f53834fcfe519b4e84f14bcbfca39c0
Pakvothe <fr.dv.ortiz@gmail.com>
2023-02-19 03:20:42 -0300

    (build) fix some build files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import * as path from "path";
-
+import viteCompression from "vite-plugin-compression";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      filter: /\.(ts|tsx|js|mjs|json|css|html)$/i,
+      algorithm: "gzip",
+      deleteOriginFile: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
