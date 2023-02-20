f25122c33acec3a8eb7dd407462e940ec1e43ecf
Pakvothe <fr.dv.ortiz@gmail.com>
2023-02-20 03:36:50 -0300

    (build) configure file compression and obfuscation

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,37 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import * as path from "path";
 import viteCompression from "vite-plugin-compression";
-// https://vitejs.dev/config/
+import { viteObfuscateFile } from "vite-plugin-obfuscator";
+
+const obfuscator_options = {
+  compact: true,
+  controlFlowFlattening: false,
+  deadCodeInjection: false,
+  debugProtection: false,
+  debugProtectionInterval: 0,
+  disableConsoleOutput: false,
+  identifierNamesGenerator: "hexadecimal",
+  log: false,
+  numbersToExpressions: false,
+  renameGlobals: false,
+  selfDefending: false,
+  simplify: true,
+  splitStrings: false,
+  stringArray: true,
+  stringArrayCallsTransform: false,
+  stringArrayCallsTransformThreshold: 0.5,
+  stringArrayEncoding: [],
+  stringArrayIndexShift: true,
+  stringArrayRotate: true,
+  stringArrayShuffle: true,
+  stringArrayWrappersCount: 1,
+  stringArrayWrappersChainedCalls: true,
+  stringArrayWrappersParametersMaxCount: 2,
+  stringArrayWrappersType: "variable",
+  stringArrayThreshold: 0.75,
+  unicodeEscapeSequence: false,
+};
+
 export default defineConfig({
   plugins: [
     react(),
@@ -11,6 +41,7 @@ export default defineConfig({
       algorithm: "gzip",
       deleteOriginFile: true,
     }),
+    viteObfuscateFile(obfuscator_options),
   ],
   resolve: {
     alias: {
